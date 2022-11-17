7bd859dfca14db3b59fb856e593daa779057b75d
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-11-17 11:42:35 -0800

    add unit conversion class

--- helpers/misc.py
+++ helpers/misc.py
@@ -4,17 +4,7 @@ import json
 from decimal import Decimal
 from datetime import date, datetime
 
-
-class TypeConvertion:
-    '''Data type conversion class.'''
-
-    def json_serial(obj) -> str | float:
-        '''JSON serializer for objects not serializable by default json code.'''
-        if isinstance(obj, (datetime, date)):
-            return obj.isoformat()
-        elif isinstance(obj, Decimal):
-            return float(obj)
-        return json.JSONEncoder.default(obj)
+from config import Settings
 
 
 class FileManagement:
@@ -29,3 +19,33 @@ class FileManagement:
         '''Read a SQL file.'''
         with open(filename, mode='r') as f:
             return f.read()
+
+
+class TypeConvertion:
+    '''Data type conversion class.'''
+
+    def json_serial(obj) -> str | float:
+        '''JSON serializer for objects not serializable by default json code.'''
+        if isinstance(obj, (datetime, date)):
+            return obj.isoformat()
+        elif isinstance(obj, Decimal):
+            return float(obj)
+        return json.JSONEncoder.default(obj)
+
+
+class UnitMeasureConversion:
+    '''Unit measure conversion class.'''
+
+    def length(value: float, unit_in: str, unit_out: str, settings: Settings) -> float:
+        '''Convert lenght measures from a unit to another.'''
+        if unit_in == 'cm' and unit_out == 'in':
+            unit = settings.UNIT_CONVERSION.length.cmIn
+        elif unit_in == 'm' and unit_out == 'ft':
+            unit = settings.UNIT_CONVERSION.length.mFt
+        return value*unit
+
+    def weight(value: float, unit_in: str, unit_out: str, settings: Settings) -> float:
+        '''Convert weight measures from a unit to another.'''
+        if unit_in == 'ton' and unit_out == 'lb':
+            unit = settings.UNIT_CONVERSION.weight.tonLb
+        return value*unit
