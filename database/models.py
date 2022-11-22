c54cd7a834b036a4d18cf53eb3d4159e4135cbbb
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-11-21 17:45:24 -0800

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,8 +1,18 @@
 '''This module defines all database tables.'''
 
-from sqlalchemy import Column, Boolean, Integer, String, DateTime
+from enum import Enum as Enumerations
+from sqlalchemy import Column, Boolean, Integer, String, DateTime, Enum
 from sqlalchemy.sql import func
+
 from database.session import Base
+
+
+class Status(Enumerations):
+    '''Define allowed status values.'''
+
+    APPROVED = 'approved'
+    DECLINED = 'declined'
+    PENDING = 'pending'
 
 
 class AdminsTable(Base):
@@ -14,5 +24,16 @@ class AdminsTable(Base):
     username = Column(String, unique=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+
+class ExampleTable(Base):
+    '''Define example as a database table.'''
+
+    __tablename__ = 'example'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    status = Column(Enum(Status))
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
