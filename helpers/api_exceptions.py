62b353ff8252328c83703f8c2b5fced5434b3b0c
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-11-28 16:34:59 -0800

    Update api_exceptions.py

--- helpers/api_exceptions.py
+++ helpers/api_exceptions.py
@@ -14,20 +14,20 @@ class ResponseValidationError(Exception):
         self.message = message
 
 
-async def request_exception_handler(request: Request, exc: RequestValidationError):  # pylint: disable=[W0613]
+async def request_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:  # pylint: disable=[W0613]
     '''Ensure that the parameters passed by the HTTP request follow the defined schemas.'''
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder(
             dict(
                 error=True,
-                message=exc.errors()
+                message=dict([(d['loc'][-1], d['msg']) for d in exc.errors()])
             )
         )
     )
 
 
-async def response_exception_handler(request: Request, exc: ResponseValidationError):  # pylint: disable=[W0613]
+async def response_exception_handler(request: Request, exc: ResponseValidationError) -> JSONResponse:  # pylint: disable=[W0613]
     '''Ensure exception responses are standardized.'''
     return JSONResponse(
         status_code=exc.status_code,
