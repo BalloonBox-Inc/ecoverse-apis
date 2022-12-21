629be15aca62a376fd95479e247954090b0e7665
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-12-21 15:12:30 -0800

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,6 @@ config['DATABASE']['POSTGRESQL']['URI'] = DataFormatter.postgresql(config['DATAB
 
 # set up supporting data
 path_json = 'supporting_data/json'  # pylint: disable=[C0103]
-
 config['ATOMIC_WEIGHT'] = FileManagement.read_file(f'{path_json}/atomic_weight.json')
 config['UNIT_CONVERSION'] = FileManagement.read_file(f'{path_json}/unit_conversion.json')
 config['PLANTATION_METRICS'] = FileManagement.read_file(f'{path_json}/plantation_metrics.json')
