cd20f3e20e4f45bb6e469ab811723da149ddad0d
mayllon <87451748+m3mayllon@users.noreply.github.com>
2023-01-18 11:20:32 -0800

    Update middleware.py

--- apis/middleware.py
+++ apis/middleware.py
@@ -3,7 +3,7 @@
 from fastapi import APIRouter
 
 from config import get_settings
-from apis.routers import admin_login, admin_mgmt, farms, nfts, proof_of_service
+from apis.routers import admin_login, admin_mgmt, farms, nfts
 
 
 api_routers = APIRouter()
@@ -23,6 +23,3 @@ api_routers.include_router(farms.router, prefix=f'{router_prefix}/farm', tags=['
 
 # NFTs
 api_routers.include_router(nfts.router, prefix=f'{router_prefix}/nft', tags=['NFTs'])
-
-# Proof Of Service
-api_routers.include_router(proof_of_service.router, prefix=router_prefix, tags=['Proof Of Service'])
