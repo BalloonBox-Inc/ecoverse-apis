031f600ce5297401052dd360aa09966f41e6aab1
mayllon <87451748+m3mayllon@users.noreply.github.com>
2023-01-18 16:52:40 -0800

    fix: product group, genus and species name

--- apis/schemas/farms.py
+++ apis/schemas/farms.py
@@ -10,16 +10,23 @@ class FarmResponse(BaseModel):
     farmId: str | None = None
     groupScheme: str | None = None
     hectareUsd: float | None = None
-    productGroup: str | None = None
-    genusName: list | None = None
-    speciesName: list | None = None
     country: str | None = None
     province: str | None = None
     latitude: float | None = None
     longitude: float | None = None
     farmSize: float | None = None  # hectares
     farmRadius: float | None = None  # meters
+    productGroup: list | None = None
+    scientificName: list | None = None
+    isFscCertified: bool | None = None
     effectiveArea: float | None = None  # hectares
     treesPlanted: int | None = None  # estimated
     plantAge: float | None = None  # average
     farmCo2y: float | None = None  # tons per hectare per year
+
+
+class FarmListResponse(BaseModel):
+    '''Response schema to /farm'''
+
+    items: list[FarmResponse] | None = []
+    total: int | None = None
