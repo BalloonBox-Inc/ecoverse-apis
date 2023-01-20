d29bb3efd55b8270a3917f73eb4de823d8aa2b87
mayllon <87451748+m3mayllon@users.noreply.github.com>
2023-01-20 15:03:54 -0800

    update: PoS satellite view

--- apis/middleware.py
+++ apis/middleware.py
@@ -3,7 +3,7 @@
 from fastapi import APIRouter
 
 from config import get_settings
-from apis.routers import admin_login, admin_mgmt, farms, nfts  # , proof_of_service
+from apis.routers import admin_login, admin_mgmt, farms, nfts, pos
 
 
 api_routers = APIRouter()
@@ -25,4 +25,4 @@ api_routers.include_router(farms.router, prefix=f'{router_prefix}/farm', tags=['
 api_routers.include_router(nfts.router, prefix=f'{router_prefix}/nft', tags=['NFTs'])
 
 # Proof Of Service
-# api_routers.include_router(proof_of_service.router, prefix=router_prefix, tags=['Proof Of Service'])
+api_routers.include_router(pos.router, prefix=router_prefix, tags=['Proof Of Service'])
---
+++ apis/routers/pos.py
@@ -0,0 +1,43 @@
+'''This module is part of the /farm FastAPI router.'''
+
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import HTMLResponse
+from sqlalchemy.orm import Session
+
+from config import get_settings
+from helpers.misc import AppSettings, FileManagement
+# from helpers.api_exceptions import ResponseValidationError # TODO: add exceptions
+from database import crud, models
+from database.session import get_db
+from security.admin import get_current_active_admin
+from security.dependencies import valid_farm_id
+from models.farm_proof_of_service import FarmProofOfService
+
+
+router = APIRouter(dependencies=[Depends(get_current_active_admin)])
+
+
+@router.get('/satellite/{farmId}', dependencies=[Depends(valid_farm_id)], status_code=status.HTTP_200_OK, response_class=HTMLResponse)
+async def show_farm_satellite_view(
+    farmId: str,
+    db: Session = Depends(get_db),
+    settings: AppSettings = Depends(get_settings)
+):
+    '''Retrieves a satellite image from a given farm.'''
+
+    farm = crud.get_object(
+        db=db,
+        table=models.FarmsTable,
+        column=models.FarmsTable.FarmId,
+        value=farmId,
+        exc_message='Unable to find farm.'
+    )
+
+    html = FarmProofOfService.satellite_mapbox(
+        farm_id=farmId,
+        lat=farm.Latitude,
+        lng=farm.Longitude,
+        settings=settings
+    )
+
+    return FileManagement.read_file(html)
