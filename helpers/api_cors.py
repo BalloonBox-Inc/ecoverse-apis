66debebb4e50b53e433671cc5321239b85693d91
mayllon <87451748+m3mayllon@users.noreply.github.com>
2023-01-27 11:09:03 -0800

    Update api_cors.py

--- helpers/api_cors.py
+++ helpers/api_cors.py
@@ -13,7 +13,7 @@ class CrossOrigin:
             CORSMiddleware,
             allow_origins=['*'],
             allow_credentials=True,
-            allow_methods=['GET', 'POST'],
-            allow_headers=['Content-Type']
+            allow_methods=['*'],
+            allow_headers=['*']
         )
         return app
